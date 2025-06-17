9cf9d0d051bf85300dabd0970aabee4e39ffc06b
Teris <teristam@gmail.com>
2025-06-17 16:25:19 +0100

    switch to use uv

--- AUTHORS.rst
+++
@@ -1,5 +0,0 @@
-============
-Contributors
-============
-
-* teristam <teristam@gmail.com>
--- examples/snakehelper_example.py
+++ examples/snakehelper_example.py
@@ -9,3 +9,4 @@ from snakehelper.SnakeIOHelper import getSnake
 (sinput, soutput) = getSnake(locals(), '../tests/make_files/workflow_common.smk', 
                                             ['../tests/processed/recording_info.pkl'],
                                             'sort_spikes',  change_working_dir=False)
+
